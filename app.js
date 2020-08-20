bda799173136636383a789891fab83c2f8bda9e1
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2020-08-20 09:48:55 -0700

    introduced user routes

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/reception', receptionRoutes);
+app.use('/user', userRoutes);
 
 
 const uri =
