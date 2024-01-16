d0387c74ba43594ae8602222d0154e5e97e8cfe4
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2024-01-16 12:54:52 +0300

    fix patient schema

--- models/patientModel.js
+++ models/patientModel.js
@@ -5,11 +5,10 @@ const Schema = mongoose.Schema;
 const patientSchema = new Schema({
   patientFirstName: { type: String, required: true },
   patientLastName: { type: String, required: true },
-  patientGender: { type: Number, required: true },
-  patientAge: { type: Date, required: true },
-}, 
-);
+  patientGender: { type: String, required: true }, // Changed from Number to String
+  patientAge: { type: Number, required: true }, // Changed from Date to Number
+});
 
-const receptionPatient = mongoose.model('receptionPatient', receptionPatientSchema);
+const receptionPatient = mongoose.model('receptionPatient', patientSchema); // Changed from receptionPatientSchema to patientSchema
 
 module.exports = receptionPatient;
