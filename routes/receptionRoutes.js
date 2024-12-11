02e8d6809aaa52037eff46989f26fceaacb0e66b
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2024-12-11 18:26:30 +0300

    feat(reception): update missing reception routes

--- routes/receptionRoutes.js
+++ routes/receptionRoutes.js
@@ -1,14 +1,23 @@
-
 const express = require('express');
 const router = express.Router();
+const receptionController = require('../controllers/receptionController'); // Updated to use the controllers
 
-// Require the controllers WHICH WE DID NOT CREATE YET!!
-const receptionController = require('../controllers/receptionController');
-
-
-
+// Home route for reception
 router.get('/', receptionController.getReceptionHome);
 
-//router.post('/addPatient', receptionController.addReceptionPatient);
+// Add a new patient at reception
+router.post('/addPatient', receptionController.addReceptionPatient);
+
+// Get all patients added at reception
+router.get('/patients', receptionController.getAllReceptionPatients);
+
+// Get details of a specific patient by ID
+router.get('/patients/:id', receptionController.getReceptionPatientById);
+
+// Update patient information at reception
+router.put('/patients/:id', receptionController.updateReceptionPatient);
+
+// Delete a patient record at reception
+router.delete('/patients/:id', receptionController.deleteReceptionPatient);
 
 module.exports = router;
